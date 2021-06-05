c567e140905eece1431a7062d3d3f5f837543237
Kavery pm <shiny.kavery@gmail.com>
2021-06-05 12:23:03 -0400

    added a react portal

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,16 @@
+import classes from './Modal.module.css';
+import React from 'react';
+const Backdrop = props =>{
+
+}
+const Overlay = props =>{
+
+}
+
+const Modal = props =>{
+return <React.Fragment>
+    <Backdrop></Backdrop>
+    <Overlay></Overlay>
+</React.Fragment>
+}
+export default Modal;
