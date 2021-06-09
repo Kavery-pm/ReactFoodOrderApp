60042aeaec2febd3e1ef47659c413aa1e33b94ef
Kavery pm <shiny.kavery@gmail.com>
2021-06-09 10:36:48 -0400

    added useEffect to add the bump class

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,31 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCart.module.css";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
-  console.log(cartCtx.items);
+  const [btnIsHighlighted, setbtnIsHighlighted] = useState(false);
+  const { items } = cartCtx;
+  useEffect(() => {
+    if (items.length === 0) return;
+    setbtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setbtnIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   const numberOfItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+  const buttonClass = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : " "
+  }`;
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClass} onClick={props.onClick}>
       <span>
         <CartIcon></CartIcon>
       </span>
